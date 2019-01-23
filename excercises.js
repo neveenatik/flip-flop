dc1d9ed84cd176fcb7a9e341aa53d42e6029153c
natik <neveen.atik@booking.com>
2019-01-23 10:55:47 +0100

    Optimized excercise 9

--- excercises.js
+++ excercises.js
@@ -120,7 +120,7 @@ function maxStockProfit(prices) {
   let maxProfit = 0;
 
   for(let i = 0; i <= prices.length -1; i++) {
-    for(let j =  i + 1 ; j < prices.length - 1; j++)
+    for(let j =  i + 1 ; j <= prices.length - 1; j++)
     if (prices[j] > prices[i] && (prices[j] - prices[i]) > maxProfit) {
       maxProfit = prices[j] - prices[i];
     }
